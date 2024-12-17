0e62e0bb8917bda37ab10aa348de0582d81369aa
rizaldy <oss@rizaldy.club>
2024-12-17 15:09:58 +0700

    feat: update schema.sql

--- server/src/db/schema.sql
+++ server/src/db/schema.sql
@@ -1,12 +1,24 @@
 CREATE TABLE IF NOT EXISTS "schema_migrations" (version varchar(128) primary key);
-CREATE TABLE scrobble (
+CREATE TABLE IF NOT EXISTS "club.flooo.scrobble" (
     id integer not null primary key autoincrement,
-    uri text not null,
+    uri text not null unique,
+    cid text not null,
     author_did text not null,
     album_cover_cid text not null,
     album_name text not null,
     artist_name text not null,
     track_name text not null,
+    created_at integer not null,
+    indexed_at integer not null
+);
+CREATE TABLE IF NOT EXISTS "club.flooo.scrobbler" (
+    id integer not null primary key autoincrement,
+    did text not null unique,
+    handler text not null,
+    avatar text,
+    banner text,
+    display_name text,
+    description text,
     created_at integer not null,
     indexed_at integer not null
 );
